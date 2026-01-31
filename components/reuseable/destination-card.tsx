4b22e2efdcc1bce79a8083d1d369981812b2fb72
thezarababy <jyokoduwa@gmail.com>
2026-01-31 21:24:15 +0100

    layout fix

--- components/reuseable/destination-card.tsx
+++ components/reuseable/destination-card.tsx
@@ -20,13 +20,9 @@ const DestinationCard: React.FC<DestinationCardProps> = ({
   commentsCount = 0,
 }) => {
   return (
-    <article className="w-full bg-white rounded-md overflow-hidden shadow-md">
-      <div className="relative w-full aspect-video">
-        <img
-          src={image}
-          alt={title}
-          className="absolute inset-0 w-full h-full object-cover"
-        />
+    <article className="w-[70%] bg-white rounded-md overflow-hidden shadow-md">
+      <div className="relative max-w-[783px] w-full aspect-video">
+        <img src={image} alt={title} className=" w-full h-full object-cover" />
       </div>
 
       <div className="p-4 space-y-4">
